b2a7c6016796776b10d4ad89f7b81e48b772fde2
Petter <petter.asla@gmail.com>
2015-09-03 09:15:38 +0200

    included a few comments

--- Information Retrieval/getArticleTitle.py
+++ Information Retrieval/getArticleTitle.py
@@ -1,4 +1,4 @@
-##### Takes the document name and return a list with the articles on the form:
+##### Takes the document name and return a list with the articles on the format:
 ##### [ ['1', '2009', 'This is a title'],
 #####   ['2', '2009', 'This is another title'],
 #####   ['3', '2010', 'Third title']
@@ -7,12 +7,13 @@
 def getArticleInfo(doc_name):
     articleInfo = []
     f = open(doc_name,"r")
-    line = f.readline() #Firste line states id, year, title of the article
-    line = f.readline()
-    counter = 0
+    line = f.readline()     #Firste line states id, year, title of the article
+    line = f.readline()     #Starts reading from the second line
+    counter = 0             #Using a counter to get previous articles in the list
     while line:
-        split_index = list_duplicates_of(line,",")
-        if ord(line[0]) < 48 or ord(line[0]) > 57 or len(split_index) != 2: # Based on the ascii value: 0-9 is from 48-57
+        split_index = list_duplicates_of(line,",")      # Returning a list with indexes based on comma (",")
+        # Checking the first char in the line based on the ascii value: 0-9 is from 48-57
+        if ord(line[0]) < 48 or ord(line[0]) > 57 or len(split_index) != 2:
             articleInfo[counter-1][2] = (articleInfo[counter-1][2].strip()) + " " + line[:-1] #Removing \n from the string
         else:
             id = line[:split_index[0]]

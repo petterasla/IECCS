25ecd37e9b6b712115e3fd7365b5b12842174b4f
Henrik Bøhler <henrik.boehler@gmail.com>
2016-02-11 16:54:21 +0100

    Multi-core baseline system.

--- System/BaselineSystem/baseline_system.py
+++ System/BaselineSystem/baseline_system.py
@@ -4,36 +4,43 @@ import os
 sys.path.append(os.path.abspath(__file__ + "/../../../"))
 ###
 
-import pandas as pd
-import System.DataProcessing.process_data as ptd
-import numpy as np
+import time
+start_time = time.time()
 
-from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline, make_union
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.svm import SVC, LinearSVC
-from sklearn.naive_bayes import MultinomialNB
+import System.DataProcessing.process_data as ptd
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.dummy import DummyClassifier
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import cross_val_predict, StratifiedKFold
 from sklearn.metrics import fbeta_score
+from sklearn.svm import LinearSVC
 
 
+# ***** LOAD DATA   *****
+# Retrieve abstracts
 abstracts = ptd.getAbstractData()
+# Retrieve labels in form of endorsement
 endorsement_data = ptd.getEndorsementData()
+
+# Convert endorsement to classes (FAVOR, AGAINST, NONE)
 target_data = []
 for endorse in endorsement_data.tolist():
     target_data.append(ptd.getAbstractStance('soft', endorse))
 
-cv = StratifiedKFold(target_data, n_folds=7, shuffle=True,
-                     random_state=1)
+cv = StratifiedKFold(target_data, n_folds=10, shuffle=True, random_state=1)
+
+# Select classifiers to use
 classifiers = [
     DummyClassifier(strategy='stratified', random_state=None, constant=None),
-    DummyClassifier(strategy='most_frequent', random_state=None, constant=None)
+    DummyClassifier(strategy='most_frequent', random_state=None, constant=None),
     #LinearSVC(C=1.0),
     #SVC(decision_function_shape='ovo', kernel='linear', shrinking=True)
     #MultinomialNB(alpha=0.5)
 ]
 
+# ***** TRAIN CLASSIFIERS   *****
 for clf in classifiers:
     print 80 * "="
     print clf
@@ -48,14 +55,14 @@ for clf in classifiers:
                          ('clf', clf)])
 
     pred_stances = cross_val_predict(pipeline, abstracts,
-                                     target_data, cv=cv)
+                                     target_data, cv=cv, n_jobs=10)
 
     print classification_report(target_data, pred_stances, digits=4)
 
     macro_f = fbeta_score(target_data, pred_stances, 1.0,
                           labels=['AGAINST', 'FAVOR', 'NONE'], average='macro')
 
-    print 'macro-average of F-score(FAVOR), F-score(AGAINST) and F-score (NONE): {:.4f}\n'.format(macro_f)
+    print 'macro-average of F-score(FAVOR), F-score(AGAINST) and F-score(NONE): {:.4f}\n'.format(macro_f)
 
 
-
+print time.time()-start_time

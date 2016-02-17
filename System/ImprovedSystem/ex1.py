068869bef08258777a78e848782423da24639aba
Henrik Bøhler <henrik.boehler@gmail.com>
2016-02-17 10:49:43 +0100

    Implemented up- and down sampling. Included log with results.

--- System/ImprovedSystem/ex1.py
+++ System/ImprovedSystem/ex1.py
@@ -16,17 +16,54 @@ from sklearn.cross_validation import cross_val_predict, StratifiedKFold
 from sklearn.metrics import fbeta_score
 from sklearn.svm import LinearSVC
 
+import pandas as pd
+
+# ***** SETTINGS   *****
+use_upsample = 1
+use_downsample = 1
+
+perform_test_on_unused_data = 1
+
+downsample_rate_favor = 3
+downsample_rate_none  = 10
+
+strength = 'soft'
 
 # ***** LOAD DATA   *****
-# Retrieve abstracts
-abstracts = ptd.getAbstractData()
-# Retrieve labels in form of endorsement
-endorsement_data = ptd.getEndorsementData()
+if use_downsample:
+    abstracts_none, labels_none, test_none, test_none_labels        = ptd.getDownsample("NONE", strength, downsample_rate_none)
+    abstracts_favor, labels_favor, test_favor, test_favor_labels    = ptd.getDownsample("FAVOR", strength, downsample_rate_none)
+    abstracts_against, labels_against                               = ptd.getAgainstAbstracts(strength)
 
-# Convert endorsement to classes (FAVOR, AGAINST, NONE)
-target_data = []
-for endorse in endorsement_data.tolist():
-    target_data.append(ptd.getAbstractStance('soft', endorse))
+    abstracts = pd.concat([abstracts_none, abstracts_favor, abstracts_against])
+    test_abstracts = pd.concat([test_none, test_favor], axis=0)
+
+    target_data = []
+    target_data.extend(labels_none)
+    target_data.extend(labels_favor)
+    target_data.extend(labels_against)
+
+    test_labels = []
+    test_labels.extend(test_none_labels)
+    test_labels.extend(test_favor_labels)
+
+else:
+    # Retrieve abstracts
+    abstracts = ptd.getAbstractData()
+    # Retrieve labels in form of endorsement
+    endorsement_data = ptd.getEndorsementData()
+
+    # Convert endorsement to classes (FAVOR, AGAINST, NONE)
+    target_data = []
+    for endorse in endorsement_data.tolist():
+        target_data.append(ptd.getAbstractStance(strength, endorse))
+
+if use_upsample:
+    againstAbstracts, againstLabels = ptd.getAgainstAbstracts(strength)
+    abstracts = pd.concat([abstracts, againstAbstracts], axis=0)
+    target_data.extend(againstLabels)
+
+
 
 cv = StratifiedKFold(target_data, n_folds=10, shuffle=True, random_state=1)
 
@@ -63,4 +100,19 @@ for clf in classifiers:
     print 'macro-average of F-score(FAVOR), F-score(AGAINST) and F-score(NONE): {:.4f}\n'.format(macro_f)
 
 
-print time.time()-start_time
+print "time = " , time.time()-start_time
+
+if use_downsample and perform_test_on_unused_data:
+    print "Testing with unused data: "
+
+    pipeline.fit(abstracts, target_data)
+
+    pred_stances_test = pipeline.predict(test_abstracts)
+
+    print classification_report(test_labels, pred_stances_test, digits=4)
+
+    macro_f = fbeta_score(test_labels, pred_stances_test, 1.0,
+                          labels=['FAVOR', 'NONE'],
+                          average='macro')
+
+    print 'macro-average of F-score(FAVOR), and F-score(NONE): {:.4f}\n'.format(macro_f)

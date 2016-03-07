31086cd14db3d1433f5115a097839cf4ba9ccfc1
Henrik Bøhler <henrik.boehler@gmail.com>
2016-03-07 19:59:44 +0100

    Bar-chart now loads data from file.

--- Web/src/app/bar-chart/components/bar-chart.js
+++ Web/src/app/bar-chart/components/bar-chart.js
@@ -1,28 +1,59 @@
-define('app/bar-chart/components/bar-chart',['require', 'knockout', 'c3', 'd3', 'app/templates/bar-chart/bar-chart'], function(require) {
+define('app/bar-chart/components/bar-chart',['require', 'knockout', 'c3', 'd3', 'app/bar-chart/data', 'app/templates/bar-chart/bar-chart'], function(require) {
   'use strict';
 
   //var ko   = require('knockout');
   var c3   = require('c3');
+  var data = require('app/bar-chart/data');
+
+  Array.prototype.getUnique = function(){
+    var u = {}, a = [];
+    for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+        continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+    }
+    return a;
+  };
 
   function ViewAbout() {
-    var chart = setTimeout(c3.generate({
+    var xValues = [];
+    var arrayLength = data.length;
+    for (var i = 0; i < arrayLength; i++) {
+        xValues.push(parseInt(data[i].Year));
+    }
+    
+    var chart = c3.generate({
+      bindto: '#BarChart',
       data: {
         columns: [
-          ['data1', 30, 200, 200, 400, 150, 250],
-          ['data2', 130, 100, 100, 200, 150, 50],
-          ['data3', 230, 200, 200, 300, 250, 250]
+          ['Favor', 1, 2],
+          ['Against', 0, 1],
+          ['None', 1, 0]
         ],
         type: 'bar',
         groups: [
-          ['data1', 'data2', 'data3']
+          ['Favor', 'Against', 'None']
         ]
+        //onclick: null
       },
       grid: {
         y: {
           lines: [{value:0}]
         }
+      },
+      axis: {
+        x: {
+          label: {
+            text: 'Year',
+            position: 'outer-center'
+          },
+          type: 'category',
+          categories: xValues.getUnique()
+        }
       }
-    }), 500);
+    });
 
     /*setTimeout(function () {
       chart.groups([['data1', 'data2', 'data3']]);
--- Web/src/app/templates/bar-chart/bar-chart.js
+++ Web/src/app/templates/bar-chart/bar-chart.js
@@ -1,7 +1,9 @@
 define('app/templates/bar-chart/bar-chart', [], function() {
   'use strict';
 
-  return  '<div id="chart" class="c3"> ' +
-
-          '</div>';
+  return  '<div class="jumbotron">' +
+            '<h2>Visualization</h2>' +
+            '<p>The chart illustrate the distribution of the classes per year in the TCP data.</p>' +
+          '</div>'+
+          '<div id="BarChart"></div>';
 });

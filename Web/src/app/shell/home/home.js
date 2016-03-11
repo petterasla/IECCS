cb1538a1ed8588601bc87985843433468804bf45
Petter Asla <petter.asla@gmail.com>
2016-03-11 17:30:34 +0100

    working test on getting data from server, note: have to use chrome or firefox due to es6 arrow func :)

--- Web/src/app/shell/home/home.js
+++ Web/src/app/shell/home/home.js
@@ -1,7 +1,24 @@
-define('app/shell/home/home', [], function() {
+define('app/shell/home/home', ['knockout','q', '$http'], function(ko, q, $http) {
   'use strict';
 
   function HomeModel() {
+
+    this.showData = ko.observableArray(["Not doing anything"]);
+
+    this.getData = () => {
+      var url = "https://ieccs.herokuapp.com/api/data";
+      this.showData("Watining for response...");
+      console.log("http req");
+      return $http.get(url)
+        .success((data) => {
+          console.log(data);
+          this.showData(data);
+        })
+        .error((err) => {
+          this.showData("Error. Something went wrong..");
+          console.log(err);
+        });
+    };
 
   }
   return HomeModel;

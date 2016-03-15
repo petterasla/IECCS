da3a524c5561835d1cae218baab8ed805c7aab4a
Petter Asla <petter.asla@gmail.com>
2016-03-15 21:36:24 +0100

    now working compact and nice, only comp errors which has not been initalizied yet..

--- Web/src/app/visualization/components/visualization-index/visualization-index.js
+++ Web/src/app/visualization/components/visualization-index/visualization-index.js
@@ -9,16 +9,16 @@ define('app/visualization/components/visualization-index/visualization-index', [
       {id: 2, type: "Map", icon: '<i class="fa fa-globe fa-2x pull-right"></i>',status: ko.observable(false)}
     ];
     this.visualModel = [
-      {id: 0, type: "Stance", status: ko.observable(false), template: `<div data-bind="component: 'bar-chart'"></div>` },
-      {id: 1, type: "Year", status: ko.observable(false), template: `<div class="jumbotron"><h2> Year option comming</h2></div>`},
-      {id: 2, type: "New Data", status: ko.observable(false), template: `<div class="jumbotron"><h2> New Data option comming</h2></div>`},
-      {id: 3, type: "Custom Search", status: ko.observable(false), template: `<div class="jumbotron"><h2> Cusomary search option comming</h2></div>`}
+      {id: 0, type: "Stance", status: ko.observable(false), repres:['bar-chart', 'graph', 'map'] },
+      {id: 1, type: "Year", status: ko.observable(false), repres:['bar-chart', 'graph', 'map']},
+      {id: 2, type: "New Data", status: ko.observable(false), repres:['bar-chart', 'graph', 'map']},
+      {id: 3, type: "Custom Search", status: ko.observable(false), repres:['bar-chart', 'graph', 'map']}
     ];
     this.allOptionsFalse = ko.observable(true);
 
     console.log(`allOptions: ${this.allOptionsFalse()}`);
 
-    this.checkOptions = (index) => {
+    this.updateVisualNavBar = (index) => {
       console.log(`Updating visual model representation: ${index}`);
       let options = false;
       this.visualModel.forEach((item) => {
@@ -49,40 +49,7 @@ define('app/visualization/components/visualization-index/visualization-index', [
         }
       });
     };
-    this.getStance = () => {
-      this.visualModel.stance(true);
-      this.visualModel.year(false);
-      this.visualModel.newData(false);
-      this.visualModel.custom(false);
-      this.visualModel.allOptionsFalse(false);
-      console.log(`getStance click: ${this.visualModel.stance()}`);
-    };
 
-    this.getYear = () => {
-      this.visualModel.stance(false);
-      this.visualModel.year(true);
-      this.visualModel.newData(false);
-      this.visualModel.custom(false);
-      this.visualModel.allOptionsFalse(false);
-      console.log(`getYear click: ${this.visualModel.year()}`);
-    };
-
-    this.getNewData = () => {
-      this.visualModel.stance(false);
-      this.visualModel.year(false);
-      this.visualModel.newData(true);
-      this.visualModel.custom(false);
-      this.visualModel.allOptionsFalse(false);
-      console.log(`getNewData click: ${this.visualModel.newData()}`);
-    };
-    this.getCustom = () => {
-      this.visualModel.stance(false);
-      this.visualModel.year(false);
-      this.visualModel.newData(false);
-      this.visualModel.custom(true);
-      this.visualModel.allOptionsFalse(false);
-      console.log(`getCutsom click: ${this.visualModel.custom()}`);
-    };
 
   };
 

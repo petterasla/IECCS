94489f04626e3465c0841524fe989fe3fb3b0028
Petter Asla <petter.asla@gmail.com>
2016-03-15 20:35:09 +0100

    more compact html representation

--- Web/src/app/visualization/components/visualization-index/visualization-index.js
+++ Web/src/app/visualization/components/visualization-index/visualization-index.js
@@ -1,26 +1,39 @@
 define('app/visualization/components/visualization-index/visualization-index', ['require', 'knockout', '$router','text!app/templates/visualization/visualization-index.html'], function(require, ko, router, template) {
   'use strict';
-
+  let self = this;
   let init = function() {
+    let self = this; //If access is needed in i.e. function, send as parameter
     this.representation = [
       {id: 0, type: "Bar Chart", icon:'<i class="fa fa-bar-chart fa-2x pull-right"></i>', status: ko.observable(true)},
       {id: 1, type: "Graph", icon: '<i class="fa fa-line-chart fa-2x pull-right"></i>',status: ko.observable(false)},
       {id: 2, type: "Map", icon: '<i class="fa fa-globe fa-2x pull-right"></i>',status: ko.observable(false)}
     ];
-    this.visualModel = {
-      stance: ko.observable(false),
-      year: ko.observable(false),
-      newData: ko.observable(false),
-      custom: ko.observable(false),
-      allOptionsFalse: ko.observable(true)
-    };
+    this.visualModel = [
+      {id: 0, type: "Stance", status: ko.observable(false), template: `<div data-bind="component: 'bar-chart'"></div>` },
+      {id: 1, type: "Year", status: ko.observable(false), template: `<div class="jumbotron"><h2> Year option comming</h2></div>`},
+      {id: 2, type: "New Data", status: ko.observable(false), template: `<div class="jumbotron"><h2> New Data option comming</h2></div>`},
+      {id: 3, type: "Custom Search", status: ko.observable(false), template: `<div class="jumbotron"><h2> Cusomary search option comming</h2></div>`}
+    ];
+    this.allOptionsFalse = ko.observable(true);
 
-    console.log(`allOptions: ${this.visualModel.allOptionsFalse()}`);
+    console.log(`allOptions: ${this.allOptionsFalse()}`);
 
-    this.checkOptions = () => {
-      let options = this.visualModel.stance && this.visualModel.year && this.visualModel.newData && this.visualModel.custom;
-      console.log(`check options ${options()}`);
-      this.visualModel.allOptionsFalse(options());
+    this.checkOptions = (index) => {
+      console.log(`Updating visual model representation: ${index}`);
+      let options = false;
+      this.visualModel.forEach((item) => {
+          if (item.id === index) {
+            item.status(true);
+            options = false;
+            console.log(`check options ${options}`);
+            this.allOptionsFalse(options);
+          }
+        else {
+            item.status(false);
+          }
+      });
+
+
     };
 
     this.updateRepresentationStatus = (index) => {

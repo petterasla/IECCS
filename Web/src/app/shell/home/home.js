a7011fb5ea8dddd6942fdbdf942c5a83f259f32d
Henrik Bøhler <henrik.boehler@gmail.com>
2016-03-16 20:55:47 +0100

    Use of single quotes.

--- Web/src/app/shell/home/home.js
+++ Web/src/app/shell/home/home.js
@@ -3,19 +3,19 @@ define('app/shell/home/home', ['knockout','q', '$http'], function(ko, q, $http) 
 
   function HomeModel() {
 
-    this.showData = ko.observableArray(["Not doing anything"]);
+    this.showData = ko.observableArray(['Not doing anything']);
 
     this.getData = () => {
-      var url = "https://ieccs.herokuapp.com/api/data";
-      this.showData("Watining for response...");
-      console.log("http req");
+      var url = 'https://ieccs.herokuapp.com/api/data';
+      this.showData('Watining for response...');
+      console.log('http req');
       return $http.get(url)
         .success((data) => {
           console.log(data);
           this.showData(data);
         })
         .error((err) => {
-          this.showData("Error. Something went wrong..");
+          this.showData('Error. Something went wrong..');
           console.log(err);
         });
     };

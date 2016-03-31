48fd08fbd595492321aaf3adca03f98820e5a99e
Petter Asla <petter.asla@gmail.com>
2016-03-31 23:01:20 +0200

    Update get meta info

--- Information Retrieval/Meta data/Wos Meta/v2/adder.py
+++ Information Retrieval/Meta data/Wos Meta/v2/adder.py
@@ -1,16 +1,13 @@
-import pandas as pd
-tcp_data = pd.read_csv("../../../tcp_abstracts.txt")
-
-
-def add(root, index):
+def add(root, id, tcp_data):
     dic = {}
 
-    dic["_id"] = tcp_data.Id.iloc[index]
-    dic["Abstract"] = tcp_data.Abstract.iloc[index].replace("|",",")
-    dic["Endorsement"] = tcp_data.Endorse.iloc[index]
-    dic["Stance"] = getStanceByEndorse(tcp_data.Endorse.iloc[index])
-    dic["Category"] = tcp_data.Cat.iloc[index]
-    dic["Title"] = tcp_data.Title.iloc[index].replace("|",",")
+    dic["_id"] = id
+    dic["Abstract"] = tcp_data.loc[id].Abstract.replace("|",",")
+    dic["Endorsement"] = tcp_data.loc[id].Endorse
+    dic["Stance"] = getStanceByEndorse(tcp_data.loc[id].Endorse)
+    dic["Category"] = tcp_data.loc[id].Cat
+    dic["Title"] = tcp_data.loc[id].Title.replace("|",",")
+    dic["Publication_year"] = tcp_data.loc[id].Year
 
     dic["Language"] = getLanguage(root)
     dic["References"] = getRefs(root)
@@ -20,7 +17,6 @@ def add(root, index):
     dic["Sub_headers"] = getSubHeader(root)
     dic["Subjects"] = getSubjects(root)
     dic["Publisher_info"] = getPublisherInfo(root)
-    dic["Publication_year"] = tcp_data.Year.iloc[index]
     dic["Publication_month"] = getPublicationInfo(root, "pubmonth")
     dic["Publication_volume"] = getPublicationInfo(root, "vol")
     dic["Publication_type"] = getPublicationInfo(root, "pubtype")
@@ -30,8 +26,6 @@ def add(root, index):
     dic["Document_type"] = getDocumentType(root)
     dic["WOS"] = getWOS(root)
 
-    print len(dic)
-    print dic
     return dic
 
 def getStanceByEndorse(endorse):
@@ -47,6 +41,7 @@ def getLanguage(root):
     try:
         return l[0].text
     except:
+        #print("No 'language' found")
         return None
 
 def getRefs(root):
@@ -54,10 +49,10 @@ def getRefs(root):
     try:
         return l[0].attrib['count']
     except:
+        #print("No 'refs' found")
         return None
 
-def getAddressSpec(list):
-    individual = None
+def getAddressSpec(list, tryNumber):
     for l in list:
         if l.tag == "country":
             country = l.text
@@ -70,23 +65,39 @@ def getAddressSpec(list):
                 org = l[1].text
             else:
                 org = l[0].text
+    individual = None
     try:
+        individual = [country, city]
+        individual = [country, city, org]
         individual = [country, city, street, org]
+        return individual
     except:
-        print("Could not add organization_spec")
-    return individual
+        if individual is None:
+            print("Organization_spec was 'None' with try {} out of 3".format(tryNumber))
+        return individual
+
 
 def getOrganizationInfo(root):
     orgs = []
     lis = root.findall(".REC/static_data/fullrecord_metadata/addresses/address_name/")
-    lis2 = root.findall(".REC/static_data/item/reprint_contact/address_spec/")
+    lis2 = root.findall("./REC/static_data/fullrecord_metadata/reprint_addresses/address_name/address_spec/")
+    lis3 = root.findall(".REC/static_data/item/reprint_contact/address_spec/")
     if len(lis) > 0:
         for li in lis:
-            orgs.append(getAddressSpec(li))
+            ret = getAddressSpec(li, 1)
+            if ret is not None:
+                orgs.append(ret)
     elif len(lis2) > 0:
-        orgs.append(getAddressSpec(lis2))
+        ret = getAddressSpec(lis2, 2)
+        if ret is not None:
+            orgs.append(ret)
+    elif len(lis3) > 0:
+        ret = getAddressSpec(lis3, 3)
+        if ret is not None:
+            orgs.append(ret)
 
     if len(orgs) == 0:
+        #print("No 'organizations' found")
         return None
     else:
         return orgs
@@ -99,6 +110,7 @@ def getKeywords(root):
     elif len(lis2) > 0:
         return [i.text for i in lis2]
     else:
+        #print("No 'keywords' found")
         return None
 
 def getHeaders(root):
@@ -106,6 +118,7 @@ def getHeaders(root):
     if len(lis) > 0:
         return [l.text for l in lis]
     else:
+        #print("No 'headers' found")
         return None
 
 def getSubHeader(root):
@@ -113,6 +126,7 @@ def getSubHeader(root):
     if len(lis) > 0:
         return [l.text for l in lis]
     else:
+        #print("No 'Sub headers' found")
         return None
 
 def getSubjects(root):
@@ -120,20 +134,23 @@ def getSubjects(root):
     if len(lis) > 0:
         return [l.text for l in lis]
     else:
+        #print("No subjects found")
         return None
 
 def getPublisherInfo(root):
     address = root.findall(".REC/static_data/summary/publishers/publisher/address_spec/full_address")[0].text
     city = root.findall(".REC/static_data/summary/publishers/publisher/address_spec/city")[0].text
     name = root.findall(".REC/static_data/summary/publishers/publisher/names/name/full_name")[0].text
-    pub_info = [i.strip() for i in address.split(",")]
-    if len(city) > 0:
-        pub_info[1] = city
-    pub_info.append(name)
+    try:
+        pub_info = [i.strip() for i in address.split(",")]
+        if len(city) > 0:
+            pub_info[1] = city
+        pub_info.append(name)
 
-    if len(pub_info) > 0:
-        return pub_info
-    else:
+        if len(pub_info) > 0:
+            return pub_info.reverse()
+    except:
+        #print("No publisher info found")
         return None
 
 def getPublicationInfo(root, key):
@@ -141,10 +158,16 @@ def getPublicationInfo(root, key):
     try:
         return dic[key]
     except:
+        #print("No publication info with key '{}' found".format(key))
         return None
 
 def getPublicationLength(root):
-    return root.findall(".REC/static_data/summary/pub_info/")[0].attrib['page_count']
+    ret = root.findall(".REC/static_data/summary/pub_info/")[0].attrib['page_count']
+    if len(ret) == 0:
+        #print("No 'page count' found")
+        return None
+    else:
+        return ret
 
 def getAuthors(root):
     lis = root.findall(".REC/static_data/summary/names/")
@@ -156,10 +179,29 @@ def getAuthors(root):
     if len(author) > 0:
         return author
     else:
+        #print("No 'author' found")
         return None
 
 def getDocumentType(root):
-    return root.findall(".REC/static_data/summary/doctypes/doctype")[0].text
+    ret = root.findall(".REC/static_data/summary/doctypes/doctype")[0].text
+    if len(ret) == 0:
+        #print("No 'document type' found")
+        return None
+    else:
+        return ret
 
 def getWOS(root):
-    return root.findall(".REC/UID")[0].text
+    ret = root.findall(".REC/UID")[0].text
+    if len(ret) == 0:
+        #print("No 'WOS' found")
+        return None
+    else:
+        return ret
+
+def getAbstract(root):
+    ret = root.findall("static_data/fullrecord_metadata/abstracts/abstract/abstract_text/")
+    try:
+        return ret[0].text
+    except:
+        return None
+
--- Information Retrieval/Meta data/Wos Meta/v2/get_info_wos_v2.py
+++ Information Retrieval/Meta data/Wos Meta/v2/get_info_wos_v2.py
@@ -21,17 +21,22 @@ def removeOperator(string):
     operators = ["AND", "OR", "NOT", "NEAR", "SAME"]
     return ' '.join([word for word in string.split(" ") if word.upper() not in operators])
 
-def queryWos(titles, years):
+def queryWos(tcp_data, start, start_sample, end_sample):
+    # Return a small list of titles, year and ids for testing
+
+    small_list_ids = tcp_data.index.values.tolist()[start_sample:end_sample]
     # Create an empty list which should contain info later
     info = []
+    not_found = []
     # Connect to Web of Science
     with WosClient(c.getUserName(), c.getPassword()) as client:
+        print("Starting the queries")
         # Looping through the titles (search parameter)
-        for i,title in enumerate(titles):
+        for i, id in enumerate(small_list_ids):
             # Replace '|' with ','
-            title = title.replace("|","")
+            title = tcp_data.loc[id].Title.replace("|","").replace("(","").replace(")","").replace("?","").replace('"', '')
             # Get year published
-            year = years[i]
+            year = tcp_data.loc[id].Year
             # Create year query with +/- 1 year
             query_string_year = 'PY=(' + str(year-1) + ' OR ' + str(year) + ' OR ' + str(year+1) + ')'
             # Check if the title contains any operators (like AND, OR, NOT)
@@ -43,42 +48,66 @@ def queryWos(titles, years):
             query_AND_operator = ' AND '
             # Create the query string
             query_string = query_string_title + query_AND_operator + query_string_year
-            print query_string
+            #print query_string
             # Perform the query on wos engine
             root = None
             try:
                 root = wos.utils.query_v2(client, query_string, count=1)
-                time.sleep(1)
             except suds.WebFault:
                 print "Suds.WebFault: Waiting 1 minute"
-                print suds.WebFault.message
+                print suds.WebFault.args
                 time.sleep(60)
             except:
-                print "Some other error occured"
-            info.append(root)
-    return info
+                print "Some other error occured, sleep 30 minute"
+                time.sleep(30)
+            if root is None:
+                # Adding tuple with id and title
+                not_found.append(id)
+                print("Did not find record with title {}".format(tcp_data.loc[id].Title))
+                print("Not found length is {}".format(len(not_found)))
+            else:
+                # Adding dictionary
+                info.append((root, id))
+                if len(info) % 25 == 0:
+                    print("Successfully retrieved is now {}".format(len(info)))
+            if (i+1) % 50 == 0:
+                print("Number of queries so far is {}. Time used is {:0.1f} minutes".format((i+1),((time.time()-start)/60.0)))
+            time.sleep(1)
+    return info, not_found
 
-def extractDataFromRoot(roots):
+def extractDataFromRoot(roots, tcp_data):
     data = []
-    for i,root in enumerate(roots):
-        data.append(adder.add(root, i))
+    for root in roots:
+        data.append(adder.add(root[0], root[1], tcp_data))
     return data
 
 
 def init():
     # Get all titles
-    tcp_data = pd.read_csv("../../../tcp_abstracts.txt")
-    # Return a small list of titles for testing
-    small_list_titles = tcp_data.Title.iloc[:2]
-    small_list_years = tcp_data.Year.iloc[:2]
+    start_time = time.time()
+    tcp_data = pd.read_csv("../../../tcp_abstracts.txt", index_col="Id")
+
     # Get a list of dicts containing data wos
-    list_of_roots_from_wos = queryWos(small_list_titles, small_list_years)
-    data = extractDataFromRoot(list_of_roots_from_wos)
+    sample_start = 1000
+    sample_end = 2000
+    list_of_roots_from_wos, not_found = queryWos(tcp_data, start_time, sample_start, sample_end)
+    data = extractDataFromRoot(list_of_roots_from_wos, tcp_data)
 
-
-    with open('wos_data.json','w') as f:
+    file_name = 'wos_data{}.json'.format(sample_end)
+    with open(file_name,'w') as f:
         json.dump(data,f)
         f.close()
 
+    not_found_file = 'not_found{}.json'.format(sample_end)
+    with open(not_found_file, 'w') as f:
+        json.dump(not_found,f)
+        f.close()
+
+    print("Time used was {:0.2f} seconds or {:0.1f} minutes".format((time.time() - start_time), ((time.time()-start_time)/60.0)))
+    print("Number of successfully retrieved records was {}".format(len(list_of_roots_from_wos)))
+    print("Number of unsuccessfully retrieved records was {}".format(len(not_found)))
+
 
 init()
+
+# TODO: Do trial runs to see if the server is overloaded...

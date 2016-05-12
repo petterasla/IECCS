15bf9898eacadba2b7c0593371ea5452e26b4097
Petter Asla <petter.asla@gmail.com>
2016-05-12 15:31:19 +0200

    updated according to results

--- System/ImprovedSystem/ex1_familiar_clf_tuning.py
+++ System/ImprovedSystem/ex1_familiar_clf_tuning.py
@@ -15,6 +15,7 @@ from sklearn.metrics import classification_report
 from sklearn.cross_validation import cross_val_predict, StratifiedKFold
 from sklearn.metrics import fbeta_score
 from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
 
 import pandas as pd
 
@@ -37,24 +38,22 @@ if use_downsample:
     data = pd.concat([sub_favor, sub_none, against])
 
 else:
-    data = pd.read_csv('../TextFiles/data/tcp_train.csv', sep='\t')
+    train_data = ptd.getTrainingData()
+    validate_data = ptd.getValidationData()
+    test_data = ptd.getTestData()
 
 if use_upsample:
     data = pd.concat([data, data[data.Stance == "AGAINST"]])
 
 
-print "None: ", len(data[data.Stance == "NONE"])
-print "Against: ", len(data[data.Stance == "AGAINST"])
-print "FAVOR: ", len(data[data.Stance == "FAVOR"])
-
-
-cv = StratifiedKFold(data.Stance, n_folds=10, shuffle=True, random_state=1)
+cv = StratifiedKFold(train_data.Stance, n_folds=10, shuffle=True, random_state=1)
 
 # Select classifiers to use
 classifiers = [
-    LinearSVC(C=0.56234132519034907),
+    #LinearSVC(C=1),
     #SVC(decision_function_shape='ovo', kernel='linear', shrinking=True)
     #MultinomialNB(alpha=0.5)
+    LogisticRegression()
 ]
 
 # ***** TRAIN CLASSIFIERS   *****
@@ -66,22 +65,61 @@ for clf in classifiers:
     # Use optimized parameters from grid_search_improved
     pipeline = Pipeline([('vect', CountVectorizer(decode_error='ignore',
                                                   analyzer='word',
-                                                  ngram_range=(1,3),
+                                                  ngram_range=(1, 1),
                                                   stop_words=None,
-                                                  max_features=None)),
-                         ('tfidf', TfidfTransformer(use_idf=False)),
+                                                  max_features=10000)),
+                         #('tfidf', TfidfTransformer(use_idf=False)),
                          ('clf', clf)])
 
-    pred_stances = cross_val_predict(pipeline, data.Abstract, data.Stance, cv=cv, n_jobs=10)
+    pred_stances = cross_val_predict(pipeline, train_data.Abstract, train_data.Stance, cv=cv, n_jobs=10)
 
-    print classification_report(data.Stance, pred_stances, digits=4)
+    print classification_report(train_data.Stance, pred_stances, digits=4)
 
-    macro_f = fbeta_score(data.Stance, pred_stances, 1.0,
+    macro_f = fbeta_score(train_data.Stance, pred_stances, 1.0,
                           labels=['AGAINST', 'FAVOR', 'NONE'],
                           average='macro')
 
     print 'macro-average of F-score(FAVOR), F-score(AGAINST) and F-score(NONE): {:.4f}\n'.format(macro_f)
 
+    print 80 * "="
+    print("Validation score")
+    print 80 * "="
+    validate_preds = cross_val_predict(pipeline, validate_data.Abstract,
+                                       validate_data.Stance)
+    print classification_report(validate_data.Stance, validate_preds, digits=4)
+
+    macro_f = fbeta_score(validate_data.Stance, validate_preds, 1.0,
+                          labels=['AGAINST', 'FAVOR', 'NONE'], average='macro')
+    print("Validation macro F-score: {:.4f}\n\n".format(macro_f))
+
+#################################
+#                               #
+#       Testing procedure       #
+#                               #
+#################################
+check_test = 1
+if check_test:
+    train_and_validation = pd.concat([train_data, validate_data])
+    for clf in classifiers:
+        print 80 * "="
+        print "TEST RESULTS FOR"
+        print clf
+        print 80 * "="
+        pipeline = Pipeline([('vect', CountVectorizer(decode_error='ignore',
+                                                      analyzer='word',
+                                                      ngram_range=(1, 1),
+                                                      stop_words= None,
+                                                      max_features=10000)),
+                             #('tfidf', TfidfTransformer(use_idf=True)),
+                             ('clf', clf)]) \
+            .fit(train_and_validation.Abstract, train_and_validation.Stance)
+
+        test_preds = pipeline.predict(test_data.Abstract)
+        print classification_report(test_data.Stance, test_preds, digits=4)
+
+        macro_f = fbeta_score(test_data.Stance, test_preds, 1.0,
+                              labels=['AGAINST', 'FAVOR', 'NONE'], average='macro')
+        print("Test macro F-score: {:.4f}".format(macro_f))
 
 print "time = " , time.time()-start_time
 

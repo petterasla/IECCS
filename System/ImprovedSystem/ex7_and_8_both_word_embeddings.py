67e7053d16b2be0186176229926342fb86bb23fa
Petter Asla <petter.asla@gmail.com>
2016-05-16 23:08:14 +0200

    glove and word2vec exps

--- System/ImprovedSystem/ex7_and_8_both_word_embeddings.py
+++ System/ImprovedSystem/ex7_and_8_both_word_embeddings.py
@@ -6,19 +6,19 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from System.DataProcessing.GloveVectorizer.glove_transformer import GloveVectorizer
+from System.DataProcessing.Word2Vec.word2vec_transformer import Word2VecVectorizer
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC, SVC
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
 from sklearn.cross_validation import cross_val_predict, StratifiedKFold
 from sklearn.metrics import fbeta_score
 from sklearn.ensemble import VotingClassifier
 
-validate = 1
-testing = 0
+
 
 data = pd.read_csv(open('../TextFiles/data/tcp_train.csv'), sep='\t', index_col=0)
 val = pd.read_csv(open('../TextFiles/data/tcp_validate.csv'), sep='\t', index_col=0)
@@ -27,6 +27,9 @@ test = pd.read_csv(open('../TextFiles/data/tcp_test.csv'), sep='\t', index_col=0
 glove_fnames = glob('../DataProcessing/GloveVectorizer/vectors/*.pkl')
 glove_ids = [fname.split('/')[-1].split('_')[0] for fname in glove_fnames]
 
+word2vec_fnames = glob('../DataProcessing/Word2Vec/vectors/word2vec_GoogleNews-vectors-negative300_tcp_abstracts.pkl')
+word2vec_ids = [fname.split('/')[-1].split('_')[0] for fname in word2vec_fnames]
+
 # *****     FINDING BEST VECTOR SPACE     *****
 for fname, glove_id in zip(glove_fnames, glove_ids):
     print 80 * '='
@@ -34,38 +37,40 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
     print 80 * '='
 
     glove_vecs = pd.read_pickle(fname)
+    word2vec_vecs = pd.read_pickle(word2vec_fnames[0])
 
     glove_clf = Pipeline([('vect', GloveVectorizer(glove_vecs)),
-                          ('clf', LogisticRegression(solver='lbfgs',
+                          ('clf', LogisticRegression(C=0.1,
+                                                     solver='lbfgs',
                                                      multi_class='multinomial',
                                                      class_weight='balanced',
                                                      ))])
 
-    char_clf = Pipeline([('vect', CountVectorizer(analyzer="word",
-                                                  ngram_range=(1, 2),
-                                                  stop_words=None,
-                                                  max_features=None,
-                                                  decode_error='ignore')),
-                         ('clf', SVC(C=5.17876863, probability=True, kernel='linear'))])
+    word2vec_pipeline = Pipeline([('vect', Word2VecVectorizer(word2vec_vecs)),
+                                  ('clf', LogisticRegression(solver='lbfgs',
+                                                                    multi_class='multinomial',
+                                                                    class_weight='balanced',
+                                                                    ))])
 
     word_clf = Pipeline([('vect', CountVectorizer(decode_error='ignore',
-                                                   analyzer='word',
-                                                   ngram_range=(2, 3),
-                                                   stop_words='english',
-                                                   max_features=50000)),
-                        ('tfidf', TfidfTransformer(use_idf=True)),
-                        ('clf', MultinomialNB(alpha=0.1, fit_prior=False))])
+                                                  analyzer='word',
+                                                  ngram_range=(2, 3),
+                                                  stop_words='english',
+                                                  max_features=50000)),
+                         ('tfidf', TfidfTransformer(use_idf=True)),
+                         ('clf', MultinomialNB(alpha=0.1, fit_prior=False))])
 
-    vot_clf = VotingClassifier(estimators=[('glove', glove_clf),
-                                           ('char', char_clf),
-                                           ('word', word_clf)],
-                               voting='hard')
+    vote_pipeline = VotingClassifier(estimators=[('glove', glove_clf),
+                                                 ('word2vec', word2vec_pipeline),
+                                                 ('mnb', word_clf)],
+                                     voting='soft')
+
     """
-    print "TRAIN"
+    print '\nTRAIN:'
     print 80 * '='
     cv = StratifiedKFold(data.Stance, n_folds=10, shuffle=True, random_state=1)
 
-    pred_stances = cross_val_predict(vot_clf, data.Abstract, data.Stance, cv=cv)
+    pred_stances = cross_val_predict(vote_pipeline, data.Abstract, data.Stance, cv=cv)
 
     print classification_report(data.Stance, pred_stances, digits=4)
 
@@ -75,34 +80,33 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
 
     print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
     """
-    print "VALIDATE"
+    print 'VALIDATE:'
     print 80 * '='
+    vote_pipeline.fit(data.Abstract, data.Stance)
 
-    vot_clf.fit(data.Abstract, data.Stance)
+    pred_stances_val = vote_pipeline.predict(val.Abstract)
 
-    pred_stances = vot_clf.predict(val.Abstract)
+    print classification_report(val.Stance, pred_stances_val, digits=4)
 
-    print classification_report(val.Stance, pred_stances, digits=4)
-
-    macro_f = fbeta_score(val.Stance, pred_stances, 1.0,
+    macro_f = fbeta_score(val.Stance, pred_stances_val, 1.0,
                           labels=['AGAINST', 'FAVOR', 'NONE'],
                           average='macro')
 
     print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
 
-
     testing = 1
     if testing:
-        print "TEST"
+
+        print 'TEST:'
         print 80 * '='
         merged = pd.concat([data, val])
-        vot_clf.fit(merged.Abstract, merged.Stance)
+        vote_pipeline.fit(merged.Abstract, merged.Stance)
 
-        pred_stances = vot_clf.predict(test.Abstract)
+        pred_stances_test = vote_pipeline.predict(test.Abstract)
 
-        print classification_report(test.Stance, pred_stances, digits=4)
+        print classification_report(test.Stance, pred_stances_test, digits=4)
 
-        macro_f = fbeta_score(test.Stance, pred_stances, 1.0,
+        macro_f = fbeta_score(test.Stance, pred_stances_test, 1.0,
                               labels=['AGAINST', 'FAVOR', 'NONE'],
                               average='macro')
 
--- System/ImprovedSystem/ex7_glove_vec_v1.py
+++ System/ImprovedSystem/ex7_glove_vec_v1.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from System.DataProcessing.GloveVectorizer.glove_transformer import GloveVectorizer
+from System.DataProcessing.Word2Vec.word2vec_transformer import Word2VecVectorizer
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
@@ -18,7 +19,7 @@ from sklearn.metrics import fbeta_score
 from sklearn.ensemble import VotingClassifier
 
 validate = 1
-testing = 0
+testing = 1
 
 data = pd.read_csv(open('../TextFiles/data/tcp_train.csv'), sep='\t', index_col=0)
 val = pd.read_csv(open('../TextFiles/data/tcp_validate.csv'), sep='\t', index_col=0)
@@ -34,49 +35,59 @@ for fname, glove_id in zip(glove_fnames, glove_ids):
     print 80 * '='
 
     glove_vecs = pd.read_pickle(fname)
-
-    glove_clf = Pipeline([('vect', GloveVectorizer(glove_vecs)),
-                          ('clf', LogisticRegression(C=0.1,
+    glove_clf = Pipeline([('vect', Word2VecVectorizer(glove_vecs)),
+                          ('clf', LogisticRegression(
                                                      solver='lbfgs',
                                                      multi_class='multinomial',
                                                      class_weight='balanced',
                                                      ))])
 
 
-    if validate:
-        glove_clf.fit(data.Abstract, data.Stance)
+    print 80 * '='
+    print 'TRAIN:'
+    print 80 * '='
+    cv = StratifiedKFold(data.Stance, n_folds=10, shuffle=True, random_state=1)
 
-        pred_stances = glove_clf.predict(val.Abstract)
+    pred_stances = cross_val_predict(glove_clf, data.Abstract, data.Stance, cv=cv)
 
-        print classification_report(val.Stance, pred_stances, digits=4)
+    print classification_report(data.Stance, pred_stances, digits=4)
 
-        macro_f = fbeta_score(val.Stance, pred_stances, 1.0,
-                              labels=['AGAINST', 'FAVOR', 'NONE'],
-                              average='macro')
-
-
-    else:
-        cv = StratifiedKFold(data.Stance, n_folds=10, shuffle=True, random_state=1)
-
-        pred_stances = cross_val_predict(glove_clf, data.Abstract, data.Stance, cv=cv)
-
-        print classification_report(data.Stance, pred_stances, digits=4)
-
-        macro_f = fbeta_score(data.Stance, pred_stances, 1.0,
-                              labels=['AGAINST', 'FAVOR', 'NONE'],
-                              average='macro')
+    macro_f = fbeta_score(data.Stance, pred_stances, 1.0,
+                          labels=['AGAINST', 'FAVOR', 'NONE'],
+                          average='macro')
 
     print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
 
+    print 80 * '='
+    print 'VALIDATE'
+    print 80 * '='
+
+    glove_clf.fit(data.Abstract, data.Stance)
+
+    pred_stances = glove_clf.predict(val.Abstract)
+
+    print classification_report(val.Stance, pred_stances, digits=4)
+
+    macro_f = fbeta_score(val.Stance, pred_stances, 1.0,
+                          labels=['AGAINST', 'FAVOR', 'NONE'],
+                          average='macro')
+
+    print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)
+
+
     if testing:
+        print 80 * '='
+        print 'TEST:', glove_id
+        print 80 * '='
         merged = pd.concat([data, val])
         glove_clf.fit(merged.Abstract, merged.Stance)
 
         pred_stances = glove_clf.predict(test.Abstract)
 
-        print classification_report(val.Stance, pred_stances, digits=4)
+        print classification_report(test.Stance, pred_stances, digits=4)
 
-        macro_f = fbeta_score(val.Stance, pred_stances, 1.0,
+        macro_f = fbeta_score(test.Stance, pred_stances, 1.0,
                               labels=['AGAINST', 'FAVOR', 'NONE'],
                               average='macro')
 
+        print 'macro-average of F-score(FAVOR) and F-score(AGAINST): {:.4f}\n'.format(macro_f)

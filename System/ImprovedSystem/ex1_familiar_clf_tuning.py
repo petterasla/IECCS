bba6207b93bfbe15c746b4c1c611408032d9a6f6
Henrik Bøhler <henrik.boehler@gmail.com>
2016-05-29 20:25:24 +0200

    Fixed error in validation for clf_tuning

--- System/ImprovedSystem/ex1_familiar_clf_tuning.py
+++ System/ImprovedSystem/ex1_familiar_clf_tuning.py
@@ -8,7 +8,7 @@ import time
 start_time = time.time()
 
 import System.DataProcessing.process_data as ptd
-import System.Utilities.helper_feature as helper
+
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
@@ -23,42 +23,21 @@ import sklearn
 
 
 
-# ***** SETTINGS   *****
-use_upsample = 0
-use_downsample = 0
-
-downsample_rate_favor = 0.5
-downsample_rate_none  = 0.3
-
 strength = 'soft'
 
 # ***** LOAD DATA   *****
-if use_downsample:
-    data = pd.read_csv('../TextFiles/data/tcp_train.csv', sep='\t')
-    sub_none = ptd.getDownsample2_0(data, "NONE", strength, downsample_rate_none)
-    sub_favor = ptd.getDownsample2_0(data, "FAVOR", strength, downsample_rate_favor)
-    against = data[data.Stance == "AGAINST"]
-
-    data = pd.concat([sub_favor, sub_none, against])
-
-else:
-    train_data = ptd.getTrainingData()
-    validate_data = ptd.getValidationData()
-    test_data = ptd.getTestData()
-
-if use_upsample:
-    data = pd.concat([data, data[data.Stance == "AGAINST"]])
-
-#train_data, validate_data, test_data = helper.lemmatizeAbstracts(train_data, validate_data, test_data)
+train_data = ptd.getTrainingData()
+validate_data = ptd.getValidationData()
+test_data = ptd.getTestData()
 
 cv = StratifiedKFold(train_data.Stance, n_folds=10, shuffle=True, random_state=1)
 
 # Select classifiers to use
 classifiers = [
-    #LinearSVC(C=1.18),
-    #SVC(C=0.1, kernel='linear')
-    #MultinomialNB(alpha=0.1, fit_prior=True)
-    #LogisticRegression(C=22.9, penalty='l2', solver='lbfgs')
+    LinearSVC(C=2.3988329190194899, multi_class='crammer_singer'),
+    #SVC(C=5.2, kernel='linear')
+    #MultinomialNB(alpha=0.63, fit_prior=True)
+    #LogisticRegression(C=22.759, penalty='l2', solver='lbfgs')
     #SGDClassifier(alpha=0.0001, loss='squared_hinge')
     #BernoulliNB(alpha=0.1, fit_prior=True)
 ]
@@ -69,11 +48,11 @@ for clf in classifiers:
 
     # Use optimized parameters from grid_search_improved
     pipeline = Pipeline([('vect', CountVectorizer(decode_error='ignore',
-                                                  analyzer='char',
-                                                  ngram_range=(2, 3),
-                                                  stop_words=None,
-                                                  max_features=50000)),
-                         #('tfidf', TfidfTransformer(use_idf=True)),
+                                                  analyzer='word',
+                                                  ngram_range=(1, 1),
+                                                  stop_words='english',
+                                                  max_features=None)),
+                         ('tfidf', TfidfTransformer(use_idf=False)),
                          ('clf', clf)])
 
     print 80 * "="
@@ -94,11 +73,10 @@ for clf in classifiers:
     print 80 * "="
     print("Validation score")
     print 80 * "="
-
     pipeline.fit(train_data.Abstract, train_data.Stance)
 
-    validate_preds = pipeline.predict(validate_data.Abstract)
-
+    validate_preds = pipeline.predict(validate_data.Abstract)#cross_val_predict(pipeline, validate_data.Abstract,
+                     #                  validate_data.Stance)
     print classification_report(validate_data.Stance, validate_preds, digits=4)
 
     macro_f = fbeta_score(validate_data.Stance, validate_preds, 1.0,
@@ -110,29 +88,29 @@ for clf in classifiers:
 #       Testing procedure       #
 #                               #
 #################################
-check_test = 0
-if check_test:
-    train_and_validation = pd.concat([train_data, validate_data])
-    for clf in classifiers:
-        print 80 * "="
-        print "TEST RESULTS FOR"
-        print clf
-        print 80 * "="
-        pipeline = Pipeline([('vect', CountVectorizer(decode_error='ignore',
-                                                      analyzer='word',
-                                                      ngram_range=(1, 3),
-                                                      stop_words='english',
-                                                      max_features=50000)),
-                             ('tfidf', TfidfTransformer(use_idf=True)),
-                             ('clf', clf)]) \
-            .fit(train_and_validation.Abstract, train_and_validation.Stance)
-
-        test_preds = pipeline.predict(test_data.Abstract)
-        print classification_report(test_data.Stance, test_preds, digits=4)
-
-        macro_f = fbeta_score(test_data.Stance, test_preds, 1.0,
-                              labels=['AGAINST', 'FAVOR', 'NONE'], average='macro')
-        print("Test macro F-score: {:.4f}".format(macro_f))
+# check_test = 0
+# if check_test:
+#     train_and_validation = pd.concat([train_data, validate_data])
+#     for clf in classifiers:
+#         print 80 * "="
+#         print "TEST RESULTS FOR"
+#         print clf
+#         print 80 * "="
+#         pipeline = Pipeline([('vect', CountVectorizer(decode_error='ignore',
+#                                                       analyzer='word',
+#                                                       ngram_range=(1, 1),
+#                                                       stop_words= None,
+#                                                       max_features=10000)),
+#                              #('tfidf', TfidfTransformer(use_idf=True)),
+#                              ('clf', clf)]) \
+#             .fit(train_and_validation.Abstract, train_and_validation.Stance)
+#
+#         test_preds = pipeline.predict(test_data.Abstract)
+#         print classification_report(test_data.Stance, test_preds, digits=4)
+#
+#         macro_f = fbeta_score(test_data.Stance, test_preds, 1.0,
+#                               labels=['AGAINST', 'FAVOR', 'NONE'], average='macro')
+#         print("Test macro F-score: {:.4f}".format(macro_f))
 
 print "time = " , time.time()-start_time
 
